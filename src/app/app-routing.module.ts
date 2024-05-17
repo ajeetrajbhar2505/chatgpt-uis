8df03276fd4563c9c7228225271fe5f17e3ec0c0
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-05-18 00:56:24 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideoPlayerComponent } from './video-player/video-player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '',redirectTo : '',pathMatch : 'full'},
+  {path : '',component : VideoPlayerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
