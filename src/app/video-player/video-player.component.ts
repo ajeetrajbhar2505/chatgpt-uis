651d7d8d1c0100579262f3373961fb5b43fc6ac7
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-05-18 03:47:15 +0530

    second commit

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrl: './video-player.component.scss'
 })
 export class VideoPlayerComponent {
+  tabs: any[] = [
+    { title: 'Lesson', isactive: true ,icon : '&#xf518;'},
+    { title: 'Chat', isactive: false ,icon : '&#xf4ad;'},
+    { title: 'Mentor', isactive: false ,icon : '&#xf007;'},
+  ]
+
+  playlists: any[] = [
+    { topic: 'Introduction', time: '0h 30min', isactive: true },
+    { topic: 'Getting Started', time: '1h 10min', isactive: false },
+    { topic: 'Learn Figma', time: '2h 20min', isactive: false },
+  ]
+
+  activetab: string = "Lesson"
+  activePlaylist: string = "Introduction"
+
+  activateTab(tab: any) {
+    this.activetab = tab.title
+  }
+
+  activatePlaylist(playlists: any) {
+    this.playlists.map(obj => {
+      obj.isactive = false
+      if (obj.topic == playlists.topic) {
+        this.activePlaylist = playlists.topic
+        obj.isactive = true
+      }
+      return obj
+    })
+  }
 
 }
