d96baf14f7800724e33d27bc38157a4ed3b9bf78
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-05-18 15:27:48 +0530

    updated and integrated video player

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,4 +1,22 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Optional,
+  ViewChild,
+  OnDestroy,
+} from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+export interface ContentControls {
+  playContent: boolean;
+  openFullscreen: boolean;
+  Rangeduration: number;
+  currentRangeDuration: number;
+  currentDuration: string;
+  duration: string;
+}
+
 
 @Component({
   selector: 'app-video-player',
@@ -6,16 +24,29 @@ import { Component } from '@angular/core';
   styleUrl: './video-player.component.scss'
 })
 export class VideoPlayerComponent {
+  contentLoaded = false;
+  videoPlaying: boolean = false
+  contentControls: ContentControls = {
+    playContent: false,
+    openFullscreen: false,
+    Rangeduration: 0,
+    currentRangeDuration: 0,
+    currentDuration: '',
+    duration: '',
+  };
+
+
+
   tabs: any[] = [
-    { title: 'Lesson', isactive: true ,icon : '&#xf518;'},
-    { title: 'Chat', isactive: false ,icon : '&#xf4ad;'},
-    { title: 'Mentor', isactive: false ,icon : '&#xf007;'},
+    { title: 'Lesson', isactive: true, icon: '&#xf518;' },
+    { title: 'Chat', isactive: false, icon: '&#xf4ad;' },
+    { title: 'Mentor', isactive: false, icon: '&#xf007;' },
   ]
 
   playlists: any[] = [
-    { topic: 'Introduction', time: '0h 30min', isactive: true ,locked : false},
-    { topic: 'Getting Started', time: '1h 10min', isactive: false ,locked : false},
-    { topic: 'Learn Figma', time: '2h 20min', isactive: false ,locked : true},
+    { topic: 'Introduction', time  : '0.40', timeTitle: '0h 30min', isactive: true, locked: false },
+    { topic: 'Getting Started', time  : '1.28', timeTitle: '1h 10min', isactive: false, locked: false },
+    { topic: 'Learn Figma', time  : '3.9', timeTitle: '2h 20min', isactive: false, locked: true },
   ]
 
   activetab: string = "Lesson"
@@ -26,6 +57,9 @@ export class VideoPlayerComponent {
   }
 
   activatePlaylist(playlists: any) {
+    if (playlists.locked) {
+      return
+    }
     this.playlists.map(obj => {
       obj.isactive = false
       if (obj.topic == playlists.topic) {
@@ -34,6 +68,128 @@ export class VideoPlayerComponent {
       }
       return obj
     })
+  }
+
+  private getContentElement(): HTMLVideoElement | null {
+    return document.querySelector<HTMLVideoElement>('#classContent');
+  }
+
+  checkContentLoaded() {
+    this.contentLoaded = true;
+    const content = this.getContentElement();
+    if (content) {
+      if (content.paused) {
+        this.contentControls.playContent = true;
+      } else {
+        this.contentControls.playContent = false;
+      }
+      this.contentControls.Rangeduration = content.duration;
+      this.contentControls.duration = this.formatTime(content.duration);
+    }
+  }
+
+  checkContinuousContentduration() {
+    const content = this.getContentElement();
+    if (content) {
+      this.contentControls.currentRangeDuration = parseInt(
+        content.currentTime.toFixed(2)
+      );
+      this.contentControls.currentDuration = this.formatTime(
+        content.currentTime.toFixed(2)
+      );
+      this.videoPlaying = true
+      if (content.paused) {
+        this.contentControls.playContent = true;
+      } else {
+        this.contentControls.playContent = false;
+      }
+    }
+  }
+
+  formatTime(duration: any) {
+    const hours = Math.floor(duration / 3600);
+    const minutes = Math.floor((duration % 3600) / 60);
+    const seconds = duration % 60;
+
+    let formattedDuration = '';
+
+    if (hours > 0) {
+      formattedDuration += `${hours}:`;
+    }
+
+    if (minutes < 10) {
+      formattedDuration += `0${minutes}:`;
+    } else {
+      formattedDuration += `${minutes}:`;
+    }
+
+    if (seconds < 10) {
+      formattedDuration += `0${Math.floor(seconds)}`;
+    } else {
+      formattedDuration += `${Math.floor(seconds)}`;
+    }
+
+    return formattedDuration;
+  }
+
+  pinFormatter(value: number) {
+    return value ? `${value}` : '0';
+  }
+
+  playPauseContent() {
+    if (!this.contentLoaded) {
+      return;
+    }
+
+    const content = this.getContentElement();
+    
+    if (content) {
+      if (content.paused) {
+        content.play();
+        this.contentControls.playContent = false;
+      } else {
+        content.pause();
+        this.contentControls.playContent = true;
+      }
+    }
+  }
+
+  skipContent(delta: number) {
+    const content = this.getContentElement();
+    if (content) {
+      content.currentTime += delta;
+      this.contentControls.currentRangeDuration = content.currentTime;
+    }
+  }
+
+
+  skipLesson(delta: any) {
+    if (delta.locked) {
+      return
+    }
+    const content = this.getContentElement();
+    if (content) {
+      const deltaValue = parseFloat(delta.time);
+      content.currentTime = deltaValue;
+      this.contentControls.currentRangeDuration = deltaValue;
+    }
+  }
+  
+
+  rewindContent(delta: number) {
+    const content = this.getContentElement();
+    if (content) {
+      content.currentTime -= delta;
+      this.contentControls.currentRangeDuration = content.currentTime;
+    }
+  }
+
+  restartContent() {
+    const content = this.getContentElement();
+    if (content) {
+      content.currentTime = 0;
+      this.contentControls.currentRangeDuration = content.currentTime;
+    }
   }
 
 }
