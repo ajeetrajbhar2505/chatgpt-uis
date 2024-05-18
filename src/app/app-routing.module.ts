aa61aef730be3044af1b9d1df7935383983ec8ea
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-05-19 00:40:39 +0530

    integrated home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VideoPlayerComponent } from './video-player/video-player.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path : '',redirectTo : '',pathMatch : 'full'},
-  {path : '',component : VideoPlayerComponent},
+  {path : '',redirectTo : 'home',pathMatch : 'full'},
+  {path : 'home',component : HomeComponent},
+  {path : 'watch',component : VideoPlayerComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideoPlayerComponent
+    VideoPlayerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
