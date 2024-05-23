ade98835e923c76b2da3ba293c6dccd6b7ecd990
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-05-24 00:34:57 +0530

    improved ui of membership plan

--- src/app/membership-plan/membership-plan.component.ts
+++ src/app/membership-plan/membership-plan.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class MembershipPlanComponent {
 
+  memvershipPlan = {
+    monthly: false,
+    yearly: false,
+    lifetime: true
+  }
+  totalprice: string = "329.90"
+
+  activateCard(membership: string,totalprice:string) {
+    this.totalprice = totalprice
+    switch (membership) {
+      case 'monthly':
+        this.memvershipPlan.monthly = true
+        this.memvershipPlan.yearly = false
+        this.memvershipPlan.lifetime = false
+        break
+      case 'yearly':
+        this.memvershipPlan.monthly = false
+        this.memvershipPlan.yearly = true
+        this.memvershipPlan.lifetime = false
+        break
+      case 'lifetime':
+        this.memvershipPlan.monthly = false
+        this.memvershipPlan.yearly = false
+        this.memvershipPlan.lifetime = true
+        break
+    }
+  }
 }
